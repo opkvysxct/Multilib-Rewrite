4ec27eb48d68aef14fd8ae620b9a19ee95a38b0c
oskar2115 <oskarpiotrkraw@gmail.com>
2024-05-25 18:57:01 +0200

    Notification system pt1

--- Multilib/Client/Components/Notifications.lua
+++ Multilib/Client/Components/Notifications.lua
@@ -1,21 +1,101 @@
+local MInstance = require(game:GetService("ReplicatedStorage").Multilib.Shared.Components.Instance)
+local TweenService = game:GetService("TweenService")
 local Lib = {}
 
 -- Core
+function Lib:AppendBigNotif(title: string, description: string, imageID: number?)
+	if self.BigNotifConfig.WasConfigured == true then
+		print(self._Queue)
+		if #self._Queue == 0 then
+			table.insert(self._Queue,{Title = title, Description = description, ImageID = imageID})
+			self:_AppendBigNotifLogic(title, description, imageID)
+		else
+			table.insert(self._Queue,{Title = title, Description = description, ImageID = imageID})
+		end
+	else
+		warn("[Multilib-" .. script.Name .. "]", "No config provided for Big Notification.")
+	end
+end
 
+function Lib:AppendSmallNotif(title: string, description: string, imageID: number?)
+	if self.SmallNotifConfig.WasConfigured == true then
+		self:_AppendSmallNotifLogic(title, description, imageID)
+	else
+		warn("[Multilib-" .. script.Name .. "]", "No config provided for Small Notification.")
+	end
+end
+
+function Lib:_AppendBigNotifLogic(title: string, description: string, imageID: number?)
+	local model, elements = MInstance:PerfectClone(self.BigNotifConfig.Template,self.BigNotifConfig.TemplatePathes)
+	elements.Title.Text = title
+	elements.Description.Text = description
+	if elements["Image"] then
+		elements.Image.Image = "rbxassetid://" .. imageID
+	end
+	model.Parent = self.BigNotifConfig.AppearWhere
+	self.BigNotifConfig.Animations.appear(model,elements)
+	task.delay(self.BigNotifConfig.Time,function()
+		self.BigNotifConfig.Animations.disappear(model,elements)
+		model:Destroy()
+		if #self._Queue > 1 then
+			local nextNotif = self._Queue[2]
+			table.remove(self._Queue,1)
+			self:_AppendBigNotifLogic(nextNotif.Title, nextNotif.Description, nextNotif.ImageID)
+		else
+			table.remove(self._Queue,1)
+		end
+	end)
+end
+
+function Lib:_AppendSmallNotifLogic(title: string, description: string, imageID: number?)
+	local model, elements = MInstance:PerfectClone(self.SmallNotifConfig.Template,self.SmallNotifConfig.TemplatePathes)
+	local heightBase = model.Size.Y.Scale
+	table.insert(self._SmallNotifs,{Model = model, Elements = elements})
+	for index, notif in ipairs(self._SmallNotifs) do
+		local heighToUse = (#self._SmallNotifs - index) * heightBase
+		self.SmallNotifConfig.Animations.giveWay(notif.Model,notif.Elements, heighToUse)
+	end
+	elements.Title.Text = title
+	elements.Description.Text = description
+	if elements["Image"] then
+		elements.Image.Image = "rbxassetid://" .. imageID
+	end
+	model.Parent = self.SmallNotifConfig.AppearWhere
+	self.SmallNotifConfig.Animations.appear(model,elements)
+	task.delay(self.SmallNotifConfig.Time,function()
+		table.remove(self._SmallNotifs,1)
+		self.SmallNotifConfig.Animations.disappear(model,elements)
+		model:Destroy()
+		for index, notif in ipairs(self._SmallNotifs) do
+			local heighToUse = (#self._SmallNotifs - index) * heightBase
+			self.SmallNotifConfig.Animations.giveWay(notif.Model,notif.Elements, heighToUse)
+		end
+	end)
+end
 
 -- Settings
-function Lib:SetSmallNotifConfig(time: number, template: GuiObject)
-	self.SmallNotifConfig.Time = time
-	self.SmallNotifConfig.Template = template
-end
-
-function Lib:SetBigNotifConfig(time: number, template: GuiObject)
+function Lib:SetBigNotifConfig(time: number, appearWhere: GuiObject, template: GuiObject, templatePathes: {GuiObject})
 	self.BigNotifConfig.Time = time
+	self.BigNotifConfig.AppearWhere = appearWhere
 	self.BigNotifConfig.Template = template
+	self.BigNotifConfig.TemplatePathes = templatePathes
+	self.BigNotifConfig.WasConfigured = true
 end
 
-function Lib:SetSmallNotifAnimations(anims: {})
-	
+function Lib:SetSmallNotifConfig(time: number, appearWhere: GuiObject, template: GuiObject, templatePathes: {GuiObject})
+	self.SmallNotifConfig.Time = time
+	self.SmallNotifConfig.AppearWhere = appearWhere
+	self.SmallNotifConfig.Template = template
+	self.SmallNotifConfig.TemplatePathes = templatePathes
+	self.SmallNotifConfig.WasConfigured = true
+end
+
+function Lib:SetBigNotifAnimations(anims: {any})
+	self.BigNotifConfig.Animations = anims
+end
+
+function Lib:SetSmallNotifAnimations(anims: {any})
+	self.SmallNotifConfig.Animations = anims
 end
 
 function Lib:SetPadding(padding: number)
@@ -25,16 +105,47 @@ end
 -- End
 function Lib:Init(comments: boolean)
 	self.Padding = 0.01
-	self.SmallNotifConfig = {
-		Time = 4,
+	self._Queue = {}
+	self.BigNotifConfig = {
+		WasConfigured = false,
+		Time = 6,
+		AppearWhere = nil,
 		Template = nil,
+		TemplatePathes = {},
 		Animations = {
+			appear = function(model: GuiObject, elements: {GuiObject})
+				model.GroupTransparency = 1
+				TweenService:Create(model,TweenInfo.new(0.5,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut),{GroupTransparency = 0}):Play()
+				task.wait(0.5)
+			end,
+			disappear = function(model: GuiObject, elements: {GuiObject})
+				TweenService:Create(model,TweenInfo.new(0.5,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut),{GroupTransparency = 1}):Play()
+				task.wait(0.5)
+			end
 		}
 	}
-	self.BigNotifConfig = {
-		Time = 6,
+	self._SmallNotifs = {}
+	self.SmallNotifConfig = {
+		WasConfigured = false,
+		Time = 4,
+		AppearWhere = nil,
 		Template = nil,
-		Animations = {}
+		TemplatePathes = {},
+		Animations = {
+			appear = function(model: GuiObject, elements: {GuiObject})
+				elements.Wrapper.Position = UDim2.fromScale(-1,0)
+				TweenService:Create(elements.Wrapper,TweenInfo.new(0.5,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut),{Position = UDim2.fromScale(0,0)}):Play()
+				task.wait(0.5)
+			end,
+			disappear = function(model: GuiObject, elements: {GuiObject})
+				TweenService:Create(elements.Wrapper,TweenInfo.new(0.5,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut),{Position = UDim2.fromScale(-1,0)}):Play()
+				task.wait(0.5)
+			end,
+			giveWay = function(model: GuiObject, elements: {GuiObject}, height: number)
+				TweenService:Create(model,TweenInfo.new(0.1,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut),{Position = UDim2.fromScale(0,height)}):Play()
+				task.wait(0.1)
+			end
+		}
 	}
 	if comments then
 		warn("[Multilib-" .. script.Name .. "]", script.Name, "Lib Loaded & safe to use.")
